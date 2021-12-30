7be1ee4dc283ca55860d0925d3293bf606dd405d
Chris Anderson <christian.c.anderson@gmail.com>
2021-12-30 10:36:43 -0500

    minor note

--- rps/__init__.py
+++ rps/__init__.py
@@ -3,5 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 logger = logging.basicConfig(level=logging.DEBUG)
 
+# TODO: add these things to a constants module?
 class_names = ['paper', 'rock', 'scissors']
 QUIT = 'QUIT'
