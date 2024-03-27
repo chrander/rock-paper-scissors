4ebf65796576fc8363c2d45aef45bd6de9f1995f
Chris Anderson <christian.c.anderson@gmail.com>
2024-03-27 00:21:07 -0400

    update bokeh server ui

--- rps/main.py
+++ rps/main.py
@@ -1,69 +1,57 @@
 import numpy as np
+import pandas as pd
 
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Slider, TextInput, NumericInput
+from bokeh.models import ColumnDataSource, NumericInput
 from bokeh.plotting import figure
 
 from rps.database.client import DatabaseClient
 from rps.constants import DATABASE_URI
 
 
-# Database client
+# Database
 db_client = DatabaseClient(database_uri=DATABASE_URI)
 
 # Set up data
-N = 200
-x = np.linspace(0, 4*np.pi, N)
-y = np.sin(x)
+
+x = np.array([])
+y = np.array([])
 source = ColumnDataSource(data=dict(x=x, y=y))
 
 
 # Set up plot
-plot = figure(height=400, width=400, title="my sine wave",
+plot = figure(height=600, width=1200, title="Human Winning Percentage",
               tools="crosshair,pan,reset,save,wheel_zoom",
-              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
+              y_range=[-0.05, 1.05], x_axis_label="Round", 
+              y_axis_label="Winning Percentage")
 
 plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
 
 # Set up widgets
 game_id_input = NumericInput(title="Game ID", value=1)
-text = TextInput(title="title", value='my sine wave')
-offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
-amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
-phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
-freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1, step=0.1)
-
 
 # Set up callbacks
-def update_title(attrname, old, new):
-    plot.title.text = text.value
-
-text.on_change('value', update_title)
-
-def update_data(attrname, old, new):
-
-    # Get the current slider values
+def update_data(attrname, old, new) -> None:
+    # Get the current Game ID values
     game_id = game_id_input.value
-    # a = amplitude.value
-    # b = offset.value
-    # w = phase.value
-    # k = freq.value
-    rounds = db_client.select_rounds_by_game_id(game_id)
+    df = pd.read_sql(f"SELECT round_id, outcome FROM rounds WHERE game_id = {game_id}", con=db_client.engine)
+    df['win_count'] = (df.outcome == 'WIN').cumsum()
+    df['loss_count'] = (df.outcome == 'LOSS').cumsum()
+    df['win_pct'] = df.win_count / (df.win_count + df.loss_count)
+    print(df)
 
     # Generate the new curve
-    x = np.linspace(0, 4*np.pi, N)
-    y = a*np.sin(k*x + w) + b
+    x = df.round_id.tolist()
+    y = df.win_pct.tolist()
 
     source.data = dict(x=x, y=y)
 
-for w in [offset, amplitude, phase, freq]:
-    w.on_change('value', update_data)
-
+game_id_input.on_change('value', update_data)
 
 # Set up layouts and add to document
-inputs = column(text, offset, amplitude, phase, freq)
+inputs = column(game_id_input)
 
-curdoc().add_root(row(inputs, plot, width=800))
+curdoc().add_root(row(inputs, plot, width=1200))
 curdoc().title = "Sliders"
