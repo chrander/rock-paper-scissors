d2ceea395960612ca4a81456c90d9609cd7883bf
Chris Anderson <christian.anderson@stresearch.com>
2024-05-06 10:52:19 -0400

    document

--- rps/gameplay/classify.py
+++ rps/gameplay/classify.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import cv2
 import numpy as np
 import torch
@@ -12,6 +14,21 @@ model = YOLO(constants.model_path)
 
 
 def process_frame(video_frame: np.array) -> torch.tensor:
+    """Preprocesses a video frame before passing to a PyTorch model
+
+    May not be required for YOLO models since the model handles this
+    stuff internally.
+
+    Parameters
+    ----------
+    video_frame : np.array
+        The frame to process
+
+    Returns
+    -------
+    torch.tensor
+        The preprocessed frame
+    """
     height, width = video_frame.shape[:2]
     img = video_frame.copy()
     # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -27,8 +44,15 @@ def process_frame(video_frame: np.array) -> torch.tensor:
     return tensor
 
 
-def get_choice_from_video() -> tuple[np.array, str]:
-    """Gets a human rock, paper, scissors choice from a video frame"""
+def get_choice_from_video() -> tuple[np.array, Union[constants.PlayerChoice, str]]:
+    """Get a human rock, paper, or scissors choice from a video stream
+
+    Returns
+    -------
+    tuple[np.array, str]
+        The image and the player choice value. If the player submitted a valid RPS choice,
+        it will be the choic value. If the player chose to quit, the string QUIT is returned.
+    """
     cap = cv2.VideoCapture(constants.VIDEO_SOURCE)
 
     while True:
@@ -39,12 +63,13 @@ def get_choice_from_video() -> tuple[np.array, str]:
         height, _ = frame.shape[:2]
         # tensor = process_frame(frame)
 
+        # Feed the image to the model and use the max probability as the player choice
         results = model(frame, imgsz=320)
         probs = results[0].probs.data.numpy()
-        # print(probs)
         pred_index = np.argmax(probs)
         prediction = constants.PLAYER_CHOICES[pred_index]
 
+        # Draw the prediction on the frame
         cv2.putText(frame, f'Your choice: {prediction.name}', (10, 50),
                     constants.font,
                     constants.choice_font_scale,
@@ -57,14 +82,14 @@ def get_choice_from_video() -> tuple[np.array, str]:
         cv2.namedWindow(constants.WINDOW_NAME, cv2.WINDOW_NORMAL)
         frame = cv2.resize(frame, constants.IMAGE_SIZE, interpolation=cv2.INTER_LINEAR)
         cv2.imshow(constants.WINDOW_NAME, frame)
-        # cv2.imshow(constants.WINDOW_NAME, img)
 
         key = cv2.waitKey(1) & 0xFF
         if key == 32:
-            # Press SPACE to end and return the current prediction
+            # Press SPACE to end the loop and return the current prediction
             value = prediction
             break
         elif key == ord('q'):
+            # Press 'q' to quit
             value = constants.QUIT
             break
 
@@ -74,77 +99,3 @@ def get_choice_from_video() -> tuple[np.array, str]:
     cv2.destroyAllWindows()
 
     return img, value
-
-
-# Load the model. TODO: make the path configurable instead of hard coded in constants
-# the_model = torch.load(constants.model_path)
-# the_model.eval()
-
-
-# def process_frame(video_frame: np.array) -> torch.tensor:
-#     height, width = video_frame.shape[:2]
-#     img = video_frame.copy()
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-#     transforms = T.Compose([
-#         T.ToTensor(),
-#         T.CenterCrop(min(height, width)),
-#         T.Resize(size=(224, 224)),
-#         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-#     ])
-
-#     tensor = transforms(img)
-#     tensor = tensor[None, :]  # Add the first dimension to the tensor so it's what PyTorch expects
-#     return tensor
-
-
-# def get_choice_from_video() -> tuple[np.array, str]:
-#     """Gets a human rock, paper, scissors choice from a video frame"""
-#     cap = cv2.VideoCapture(source)
-
-#     while(True):
-#         # Capture the video frame by frame
-#         _, frame = cap.read()
-
-#         # Resize to appropriate dimensions
-#         # frame = cv2.resize(frame, constants.IMAGE_SIZE)
-
-#         img = frame.copy()  # Copy so that we don't return something with text on it
-#         height, _ = frame.shape[:2]
-#         tensor = process_frame(frame)
-
-#         outputs = the_model(tensor)
-#         _, preds = torch.max(outputs, 1)
-#         prediction = constants.PLAYER_CHOICES[preds[0]]
-
-#         cv2.putText(frame, f'Your choice: {prediction.name}', (10, 50),
-#                     constants.font,
-#                     constants.choice_font_scale,
-#                     constants.choice_font_color,
-#                     constants.choice_font_thickness,
-#                     constants.font_line_type)
-#         cv2.putText(frame, 'Press SPACE to select choice, q to quit', (10, height-10),
-#                     constants.font, 1, constants.choice_font_color, 2, constants.font_line_type)
-#         # Display the resulting frame
-#         cv2.namedWindow(constants.WINDOW_NAME, cv2.WINDOW_NORMAL)
-#         frame = cv2.resize(frame, constants.IMAGE_SIZE, interpolation=cv2.INTER_LINEAR)
-#         cv2.imshow(constants.WINDOW_NAME, frame)
-
-
-#         key = cv2.waitKey(1) & 0xFF
-#         if key == 32:
-#             # Press SPACE to end and return the current prediction
-#             value = prediction
-#             break
-#         elif key == ord('q'):
-#             value = constants.QUIT
-#             break
-
-#     # Release the capture object
-#     cap.release()
-#     # Destroy all the windows
-#     cv2.destroyAllWindows()
-
-#     return img, value
-
-
--- rps/main.py
+++ rps/main.py
@@ -1,3 +1,9 @@
+"""This file handles bokeh dashboard visualization of game statistics
+
+To conform to `bokeh serve` signature, this file needs to be named `main.py`.
+It will be launched when `bokeh serve` is executed.
+"""
+
 import logging
 
 from bokeh.io import curdoc
@@ -13,18 +19,22 @@ from rps.constants import DATABASE_URI
 
 logger = logging.getLogger(__name__)
 
+# Colors of historical RPS choice boxes (fill colors)
 CHOICE_COLORS = {
     "PAPER": "#ece7f2",
     "ROCK": "#9ebcda",
     "SCISSORS": "#8856a7"
 }
 
+# Colors of historical RPS choice boxes (line colors)
+# Player 1 is the "human", so wins are green/losses are red
 P1_OUTCOME_COLORS = {
     "WIN": "green",
     "LOSS": "firebrick",
     "DRAW": "blue"
 }
 
+# Player 1 is the computer, so wins are red/losses are green
 P2_OUTCOME_COLORS = {
     "WIN": "firebrick",
     "LOSS": "green",
@@ -33,6 +43,19 @@ P2_OUTCOME_COLORS = {
 
 
 def get_data(game_id: int) -> pd.DataFrame:
+    """Gets data from the database to display game history
+
+    Parameters
+    ----------
+    game_id : int
+        The game ID for which to retrieve data
+
+    Returns
+    -------
+    pd.DataFrame
+        Game statistics to be used in visualizations
+    """
+    # Data to use in the human win percentage plot
     win_pct_df = pd.read_sql(f"SELECT outcome FROM rounds WHERE game_id = {game_id}",
                              con=db_client.engine)
     win_pct_df["win_count"] = (win_pct_df.outcome == "WIN").cumsum()
@@ -42,7 +65,7 @@ def get_data(game_id: int) -> pd.DataFrame:
     win_pct_df["win_pct"].fillna(0.0)
     win_pct_df["round"] = np.arange(len(win_pct_df)) + 1
 
-    # Get outcome data
+    # Data to use in the counts of rounds, wins, losses, and draws
     outcome_df = pd.read_sql("SELECT win_count, loss_count, draw_count, "
                              "win_count+loss_count+draw_count AS games_count "
                              f"FROM game_stats WHERE game_id = {game_id}",
@@ -50,7 +73,7 @@ def get_data(game_id: int) -> pd.DataFrame:
     outcome_df["x"] = 0
     outcome_df["y"] = 0
 
-    # Get player choice data
+    # Data to use in display of last 10 human and computer choices
     choices_df = pd.read_sql("SELECT timestamp, player1_choice AS p1, player2_choice AS p2, "
                              f"outcome FROM rounds WHERE game_id = {game_id} "
                              "ORDER BY timestamp DESC LIMIT 10",
@@ -72,17 +95,18 @@ def get_data(game_id: int) -> pd.DataFrame:
 # Database
 db_client = DatabaseClient(database_uri=DATABASE_URI)
 
-# Get the most recent game to start
+# Get the most recent game ID to start--can be changed interactively in the display
 most_recent_game = db_client.select_most_recent_game()
 
-# Set up data
+# Set up initial data
 win_pct_df, outcome_df, choices_df = get_data(most_recent_game.game_id)
 win_pct_source = ColumnDataSource(data=win_pct_df)
 outcome_source = ColumnDataSource(data=outcome_df)
 choices_source = ColumnDataSource(data=choices_df)
 
-# ---------------------------- #
-# Set up Winning Fraction plot #
+# ----------------------------
+# Set up Winning Fraction plot
+#
 TOOLTIPS = """
     <font face="Arial" size="6"><strong>Round:</strong> @round</font><br>
     <font face="Arial" size="6"><strong>Winning Pct.:</strong> @win_pct{0.000}</font>
@@ -105,8 +129,9 @@ wp_plot.title.align = "center"
 wp_plot.title.text_color = "navy"
 wp_plot.title.text_font_size = "28pt"
 
-# ---------------------------- #
-# Set up "scoreboard" plots
+# ----------------------------
+# Set up "scoreboard" plots of round outcomes (num. rounds, wins, losses, draws)
+#
 figure_args = {
     "height": 350,
     "width": 550,
@@ -145,9 +170,9 @@ for p in [games_plot, wins_plot, losses_plot, draws_plot]:
     p.title.text_color = "navy"
     p.title.text_font_size = "44pt"
 
-# ---------------------------- #
-# Set up recent choices plot
-
+# ----------------------------
+# Set up recent choices plot; shows most recent human and computer choices
+#
 c1 = figure(height=200, width=2200, y_range=(-0.6, 0.6), title="Most Recent Human Choices",
             margin=(10, 10, 10, 10))
 c1.rect(x="x", y="y", fill_color="p1_color", line_color="p1_line_color", source=choices_source,
@@ -175,6 +200,7 @@ for p in [c1, c2]:
 
 
 # Set up callbacks
+# Need a wrapper function that uses the input callback signature for when game ID is changed
 def update_data_on_game_id_change(attrname, old, new) -> None:
     """Update when game ID input changes"""
     update_data()
